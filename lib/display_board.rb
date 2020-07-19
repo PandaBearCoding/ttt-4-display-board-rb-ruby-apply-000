701c6eb945bbbc6bd968709d621a9bcce0b3e45b
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-19 01:01:17 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(space)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end 
+
+def display_board(space)
+  puts "#{space[0]}, #{space[1]}, #{space[2]}, #{space[3]}, #{space[4]}, #{space[5]}, #{space[6]}, #{space[7]}, #{space[8]}"
+end
+
+space = (["top_left", "top_middle", "top_right", "middle_left", "middle_middle", "middle)_right", "bottom_left", "bottom_middle", "bottom_right"])
