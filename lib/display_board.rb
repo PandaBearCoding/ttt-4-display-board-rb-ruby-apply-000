05ab4ff041021e23e68e64684a4bd8520d49920e
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-19 01:03:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,4 +3,4 @@ display_board(board)
  puts "#{board[0]}, #{board[1]}, #{board[2]}, #{board[3]}, #{board[4]}, #{board[5]}, #{board[6]}, #{board[7]}, #{board[8]}"
  end
  
- space = (["top_left", "top_middle", "top_right", "middle_left", "middle_middle", "middle)_right", "bottom_left", "bottom_middle", "bottom_right"])
+ board = (["top_left", "top_middle", "top_right", "middle_left", "middle_middle", "middle)_right", "bottom_left", "bottom_middle", "bottom_right"])
