668a1d7186fd3e5c72d8e955215c35e30346bc4e
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-19 01:09:42 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 #display_board(board)
  #puts "#{board[0]}, #{board[1]}, #{board[2]}, #{board[3]}, #{board[4]}, #{board[5]}, #{board[6]}, #{board[7]}, #{board[8]}"
- end
+ #end
  
- board = [" "," "," "," "," "," "," "," "," "]
+ #board = [" "," "," "," "," "," "," "," "," "]
 
 
 def display_board(board)
@@ -13,6 +13,6 @@ def display_board(board)
   puts "   |   |   "
 end
 
-board = 
+board = [" X ", "   ", " O ", "  "]
   
   
