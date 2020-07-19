bfa7e30b3b6dcd0e630cb6f13561113699b28dbf
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-19 01:03:09 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,15 +1,6 @@
-
-
-def display_board(space)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end 
-
-def display_board(space)
-  puts "#{space[0]}, #{space[1]}, #{space[2]}, #{space[3]}, #{space[4]}, #{space[5]}, #{space[6]}, #{space[7]}, #{space[8]}"
-end
-
-space = (["top_left", "top_middle", "top_right", "middle_left", "middle_middle", "middle)_right", "bottom_left", "bottom_middle", "bottom_right"])
+board = [" "," "," "," "," "," "," "," "," "]
+display_board(board)
+ puts "#{space[0]}, #{space[1]}, #{space[2]}, #{space[3]}, #{space[4]}, #{space[5]}, #{space[6]}, #{space[7]}, #{space[8]}"
+ end
+ 
+ space = (["top_left", "top_middle", "top_right", "middle_left", "middle_middle", "middle)_right", "bottom_left", "bottom_middle", "bottom_right"])
