167635e2b4fd0c8e46e86a65c0ed7683f5a58841
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-19 01:05:11 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
-board = [" "," "," "," "," "," "," "," "," "]
 display_board(board)
  puts "#{board[0]}, #{board[1]}, #{board[2]}, #{board[3]}, #{board[4]}, #{board[5]}, #{board[6]}, #{board[7]}, #{board[8]}"
  end
  
- board = (["top_left", "top_middle", "top_right", "middle_left", "middle_middle", "middle)_right", "bottom_left", "bottom_middle", "bottom_right"])
+ board = [" "," "," "," "," "," "," "," "," "]
+
